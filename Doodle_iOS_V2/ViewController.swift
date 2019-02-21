ce94ea2481a5f23bbe0ff6e5185b5c1b18c35264
신동규 <shindk8659@naver.com>
2019-02-21 19:02:13 +0900

    Master Branch 글적 프로젝트 생성

---
+++ Doodle_iOS_V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doodle_iOS_V2
+//
+//  Created by 신동규 on 2/21/19.
+//  Copyright © 2019 Doodle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '11.0'
+
+target 'Doodle_iOS_V2' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Doodle_iOS_V2
+   pod 'Alamofire'
+
+end
